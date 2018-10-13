224ce87c993dd2baade86951ede706ea4e332686
rachitGulati <rachitgulati26@gmail.com>
2018-10-13 10:34:45 +0530

    Fix CSS and dev mode of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
+const env = process.env.NODE_ENV
+
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
 });
 
-module.exports = {
+module.exports = (env, options) => {
+  console.log("WEBPACK ENV", options.mode);
+  return {
     output: {
-        publicPath: './',
+        publicPath: options.mode === 'prodcition' ? './' : '',
         path: path.resolve('docs'),
         filename: 'bundled.js'
     },
-  module: {
-    rules: [
-      { test: /\.json$/, 
-        loaders: ['json-loader']
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+    module: {
+      rules: [
+        { test: /\.json$/, 
+          loaders: ['json-loader']
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.(css|scss)$/,
+          use: ["style-loader", "css-loader"]
         }
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  plugins: [htmlWebpackPlugin]
+      ]
+    },
+    plugins: [htmlWebpackPlugin]
+  }
 };
